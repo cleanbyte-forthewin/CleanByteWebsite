9f4103e28857b7544dea8d2fab025d8f64bb158b
Mingodando <thebestballer182@gmail.com>
2026-04-12 01:33:33 +0700

    Remove openCity function and window.onload behavior
    Removed the openCity function and its auto-load functionality.

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-function openCity(cityName) {
-    // Hide all tab content
-    var tabcontent = document.getElementsByClassName("tabcontent");
-    for (var i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
-    
-    // Show the selected tab
-    var selectedTab = document.getElementById(cityName);
-    if (selectedTab) {
-        selectedTab.style.display = "block";
-    }
-}
 
-// Automatically load Home tab when page loads
-window.onload = function() {
-    openCity('Home');
-};
